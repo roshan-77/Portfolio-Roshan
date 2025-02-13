d164d2d3ac586a8c1f94841a92e779857b74c86a
roshan-77 <roshan.khadka.u@gmail.com>
2025-02-13 14:20:19 +1100

    Modal added

--- js/main.js
+++ js/main.js
@@ -1,141 +1,162 @@
 (function ($) {
-	"use strict";
-	var nav = $('nav');
+  "use strict";
+  var nav = $("nav");
   var navHeight = nav.outerHeight();
-  
-  $('.navbar-toggler').on('click', function() {
-    if( ! $('#mainNav').hasClass('navbar-reduce')) {
-      $('#mainNav').addClass('navbar-reduce');
+
+  $(".navbar-toggler").on("click", function () {
+    if (!$("#mainNav").hasClass("navbar-reduce")) {
+      $("#mainNav").addClass("navbar-reduce");
     }
-  })
+  });
 
   // Preloader
-  $(window).on('load', function () {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
-        $(this).remove();
-      });
+  $(window).on("load", function () {
+    if ($("#preloader").length) {
+      $("#preloader")
+        .delay(100)
+        .fadeOut("slow", function () {
+          $(this).remove();
+        });
     }
   });
 
   // Back to top button
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
+      $(".back-to-top").fadeIn("slow");
     } else {
-      $('.back-to-top').fadeOut('slow');
+      $(".back-to-top").fadeOut("slow");
     }
   });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
     return false;
   });
 
-	/*--/ Star ScrollTop /--*/
-	$('.scrolltop-mf').on("click", function () {
-		$('html, body').animate({
-			scrollTop: 0
-		}, 1000);
-	});
+  /*--/ Star ScrollTop /--*/
+  $(".scrolltop-mf").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
 
-	/*--/ Star Counter /--*/
-	$('.counter').counterUp({
-		delay: 15,
-		time: 2000
-	});
+  /*--/ Star Counter /--*/
+  $(".counter").counterUp({
+    delay: 15,
+    time: 2000,
+  });
 
-	/*--/ Star Scrolling nav /--*/
-	$('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: (target.offset().top - navHeight + 5)
-				}, 1000, "easeInOutExpo");
-				return false;
-			}
-		}
-	});
+  /*--/ Star Scrolling nav /--*/
+  $('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - navHeight + 5,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
 
-	// Closes responsive menu when a scroll trigger link is clicked
-	$('.js-scroll').on("click", function () {
-		$('.navbar-collapse').collapse('hide');
-	});
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
-	// Activate scrollspy to add active class to navbar items on scroll
-	$('body').scrollspy({
-		target: '#mainNav',
-		offset: navHeight
-	});
-	/*--/ End Scrolling nav /--*/
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: navHeight,
+  });
+  /*--/ End Scrolling nav /--*/
 
-	/*--/ Navbar Menu Reduce /--*/
-	$(window).trigger('scroll');
-	$(window).on('scroll', function () {
-		var pixels = 50; 
-		var top = 1200;
-		if ($(window).scrollTop() > pixels) {
-			$('.navbar-expand-md').addClass('navbar-reduce');
-			$('.navbar-expand-md').removeClass('navbar-trans');
-		} else {
-			$('.navbar-expand-md').addClass('navbar-trans');
-			$('.navbar-expand-md').removeClass('navbar-reduce');
-		}
-		if ($(window).scrollTop() > top) {
-			$('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
-		} else {
-			$('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
-		}
-	});
+  /*--/ Navbar Menu Reduce /--*/
+  $(window).trigger("scroll");
+  $(window).on("scroll", function () {
+    var pixels = 50;
+    var top = 1200;
+    if ($(window).scrollTop() > pixels) {
+      $(".navbar-expand-md").addClass("navbar-reduce");
+      $(".navbar-expand-md").removeClass("navbar-trans");
+    } else {
+      $(".navbar-expand-md").addClass("navbar-trans");
+      $(".navbar-expand-md").removeClass("navbar-reduce");
+    }
+    if ($(window).scrollTop() > top) {
+      $(".scrolltop-mf").fadeIn(1000, "easeInOutExpo");
+    } else {
+      $(".scrolltop-mf").fadeOut(1000, "easeInOutExpo");
+    }
+  });
 
-	/*--/ Star Typed /--*/
-	if ($('.text-slider').length == 1) {
-    var typed_strings = $('.text-slider-items').text();
-		var typed = new Typed('.text-slider', {
-			strings: typed_strings.split(','),
-			typeSpeed: 80,
-			loop: true,
-			backDelay: 1100,
-			backSpeed: 30
-		});
-	}
+  /*--/ Star Typed /--*/
+  if ($(".text-slider").length == 1) {
+    var typed_strings = $(".text-slider-items").text();
+    var typed = new Typed(".text-slider", {
+      strings: typed_strings.split(","),
+      typeSpeed: 80,
+      loop: true,
+      backDelay: 1100,
+      backSpeed: 30,
+    });
+  }
 
-	/*--/ Testimonials owl /--*/
-	$('#testimonial-mf').owlCarousel({
-		margin: 20,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-			}
-		}
-	});
+  /*--/ Testimonials owl /--*/
+  $("#testimonial-mf").owlCarousel({
+    margin: 20,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+  });
 
-	/*---- Skill ----*/
-	$('.skill').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
-
+  /*---- Skill ----*/
+  $(".skill").waypoint(
+    function () {
+      $(".progress .progress-bar").each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%");
+      });
+    },
+    { offset: "80%" }
+  );
 })(jQuery);
 
-const gitProject = $('.github-projects')
+const gitProject = $(".github-projects");
 
-$.ajax({url : "https://api.github.com/users/roshan-77/repos",
-	method: "GET"
-}).then((result)=>{console.log(result)
+$.ajax({
+  url: "https://api.github.com/users/roshan-77/repos",
+  method: "GET",
+}).then((result) => {
+  console.log(result);
 
-	for(var i=0; i<=result.length; i++){
-		if(result[i].stargazers_count !== 0){
-			let article = `<div class="col-md-4">
+  for (var i = 0; i <= result.length; i++) {
+    if (result[i].stargazers_count !== 0) {
+      let article = `<div class="col-md-4">
           <div class="work-box" >
-            <div class= "work-box-m" data-bs-toggle="modal" data-bs-target="#portfolio-modal-1" >
+            <div class= "work-box-m"  data-toggle="modal" data-bs-toggle="modal" data-bs-target="#portfolio-modal-${i}" >
               <div class="work-img">
-                <img src="https://raw.githubusercontent.com/roshan-77/${result[i].name}/${result[i].default_branch}/Thumbnail/thumbnail.png" alt="Thumbnail" class="img-fluid">
+                <img src="https://raw.githubusercontent.com/roshan-77/${
+                  result[i].name
+                }/${
+        result[i].default_branch
+      }/Thumbnail/thumbnail.png" alt="Thumbnail" class="img-fluid">
               </div>
               <div class="work-content">
                 <div class="row">
@@ -143,40 +164,91 @@ $.ajax({url : "https://api.github.com/users/roshan-77/repos",
                     <h2 class="w-title">${result[i].name}</h2>
                   </div>
                   <div class="w-portfolio-links">
-                    ${result[i].has_pages === true ? `<a href="https://roshan-77.github.io/${result[i].name}/" target="_blank"><button class="button">Website</button></a>` : '<div></div>'}
-                    <a href="https://github.com/roshan-77/${result[i].name}" target="_blank"><button class="button">Source Code</button></a>
+                    ${
+                      result[i].has_pages === true
+                        ? `<a href="https://roshan-77.github.io/${result[i].name}/" target="_blank"><button class="button">Website</button></a>`
+                        : "<div></div>"
+                    }
+                    <a href="https://github.com/roshan-77/${
+                      result[i].name
+                    }" target="_blank"><button class="button">Source Code</button></a>
+					<button class="button" data-toggle="modal" data-target="#portfolio-modal-${i}">
+						Expand
+					</button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>`
-			gitProject.append(article)
-		}
+        </div>
 
-	}
-})
+		
+		
+		<!-- Modal -->
+      <div class="modal fade" id="portfolio-modal-${i}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">${
+                result[i].name
+              }</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              ${
+                result[i].description === null
+                  ? "No description found"
+                  : result[i].description
+              }
+            </div>
+            <div class="modal-footer">
+			${
+        result[i].has_pages === true
+          ? `<a href="https://roshan-77.github.io/${result[i].name}/" target="_blank"><button class="button">Website</button></a>`
+          : "<div></div>"
+      }
+			  <a href="https://github.com/roshan-77/${
+          result[i].name
+        }" target="_blank"><button class="button">Source Code</button></a>
+            </div>
+          </div>
+        </div>
+      </div>
+		
+		`;
+      gitProject.append(article);
+    }
+  }
+});
 
 // Email Js
-function sendMail(params){
-	var tempParams= {
-	  from_name:document.getElementById('fromName').value,
-	  from_email: document.getElementById('fromEmail').value,
-	  subject: document.getElementById('subject').value,
-	  message:document.getElementById('msg').value,
-	}
-	if(tempParams.from_name !="" && tempParams.from_email !="" && tempParams.subject !="" && tempParams.subject !=""){
-		emailjs.send('service_2hs31fo', 'template_6cvxcsf', tempParams)
-		.then(function(res){
-		console.log("success", res.status)
-		}).then(()=>alert("Email Sent!!"))
-		.then(()=>{
-		document.getElementById('fromName').value = ''
-		document.getElementById('fromEmail').value = ''
-		document.getElementById('subject').value =''
-		document.getElementById('msg').value =''
-		})
-	}else{
-		
-	}
+function sendMail(params) {
+  var tempParams = {
+    from_name: document.getElementById("fromName").value,
+    from_email: document.getElementById("fromEmail").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("msg").value,
+  };
+  if (
+    tempParams.from_name != "" &&
+    tempParams.from_email != "" &&
+    tempParams.subject != "" &&
+    tempParams.subject != ""
+  ) {
+    emailjs
+      .send("service_2hs31fo", "template_6cvxcsf", tempParams)
+      .then(function (res) {
+        console.log("success", res.status);
+      })
+      .then(() => alert("Email Sent!!"))
+      .then(() => {
+        document.getElementById("fromName").value = "";
+        document.getElementById("fromEmail").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("msg").value = "";
+      });
+  } else {
   }
+}
